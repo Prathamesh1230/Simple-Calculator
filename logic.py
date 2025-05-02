8d9d911663eb4ff5648d464370f44f2619cc1c1f
Prathamesh1230 <prathamesh1470@gmail.com>
2025-05-02 13:49:07 +0530

    Deleted logic.py

--- logic.py
+++
@@ -1,8 +0,0 @@
-def evaluate_expression(expression):
-    try:
-        result = str(eval(expression))
-        return result
-    except ZeroDivisionError:
-        return "Error: Division by Zero"
-    except Exception:
-        return "Error"
